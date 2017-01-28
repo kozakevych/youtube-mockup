90a15eb2439cded427d6e2b0170d2736bebc7450
kozakevych <andriikozakevych@gmail.com>
2017-01-28 22:39:19 +0200

    fixed some bugs

--- final-task/js/app.js
+++ final-task/js/app.js
@@ -6,11 +6,30 @@ $(function (){
 			$(".under-nav").css("padding-left", 0);
 			$(".main-body").css("padding-left", 0);
 			$("footer").css("padding-left", 32);
+		} else if ( ( $(window).width() < 768 )) {
+			$(".side-nav").show();
 		} else {
 			$(".side-nav").show();
 			$(".under-nav").css("padding-left", 230);
 			$(".main-body").css("padding-left", 230);
 			$("footer").css("padding-left", 260);
+		}
+	});
+
+	// run test on initial page load
+    //checkSize();
+    // run test on resize of the window
+    $(window).resize(function(){
+	  	if ( $(window).width() < 752 ){
+		    $(".yt-logo").attr("src", "img/favicon.png");
+		    $(".top-right").hide();	
+			$(".side-nav").hide();
+			$(".under-nav").css("padding-left", 0);
+			$(".main-body").css("padding-left", 0);
+			$("footer").css("padding-left", 32);
+		} else {
+		    $(".yt-logo").attr("src", "img/youtube-logo.png");
+		    $(".top-right").show();	
 		}
 	});
 
