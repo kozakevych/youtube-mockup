ef4c742c707039d60932243ed2dae629bc303237
kozakevych <andriikozakevych@gmail.com>
2017-02-02 16:30:39 +0200

    video rwd upd

--- final-task/js/app-video.js
+++ final-task/js/app-video.js
@@ -17,33 +17,56 @@ $(function (){
 	});
 
 
-  	if ( $(window).width() < 752 ){
+  	if ( $(window).width() < 862 ){
 	    $("#yt-logo-top").attr("src", "img/favicon.png");
 	    $("#video-frame").attr("width","560");
 	    $("#video-frame").attr("height","315");
 	    $(".top-right").hide();
 	} else {
 	    $("#yt-logo-top").attr("src", "img/youtube-logo.png");
+	    $("#video-frame").attr("width","853");
+	    $("#video-frame").attr("height","480");
 	    $(".top-right").show();	
 	}
 
 
 
 	$( $(window).resize(function(){
-		if ( $(window).width() < 752 ){
+		if ( $(window).width() < 862 ){
 		    $("#yt-logo-top").attr("src", "img/favicon.png");
 		    $("#video-frame").attr("width","560");
+		    $("#video-frame").attr("height","315");
 		    $(".top-right").hide();
 		} else {
 		    $("#yt-logo-top").attr("src", "img/youtube-logo.png");
-		    $(".top-right").show();
-		    $("#video-frame").attr("height","315");
 		    $("#video-frame").attr("width","853");
 		    $("#video-frame").attr("height","480");
+		    $(".top-right").show();
 		}
 	}))
 
+	if ( $(window).width() < 450 ){
+	    $("#video-frame").attr("width","426");
+	    $("#video-frame").attr("height","240");
+	} else if ( $(window).width() < 862 ){	
+	    $("#video-frame").attr("width","560");
+	    $("#video-frame").attr("height","315");
+	}
 
+
+
+	$( $(window).resize(function(){
+		if ( $(window).width() < 450 ){
+		    $("#video-frame").attr("width","426");
+	    	$("#video-frame").attr("height","240");
+		} else if ( $(window).width() < 862 ){
+		    $("#video-frame").attr("width","560");
+		    $("#video-frame").attr("height","315");
+		}
+	}))
+// 450
+//426
+//240
 })
 
 
